2e7571bb970ad9e9c4bee30b4e7de47975663927
deploy <deploy@trust.supplymax.net>
2026-02-09 01:55:49 +0100

    feat: add production settings (STATIC_ROOT + CSRF + whitenoise)

---
+++ backend/config/settings_prod.py
@@ -0,0 +1,19 @@
+from .settings import *  # noqa
+
+DEBUG = False
+ALLOWED_HOSTS = ["trust.supplymax.net", "localhost", "127.0.0.1"]
+
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+STORAGES = {
+  "staticfiles": {
+    "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+  }
+}
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_TRUSTED_ORIGINS = ["https://trust.supplymax.net"]
