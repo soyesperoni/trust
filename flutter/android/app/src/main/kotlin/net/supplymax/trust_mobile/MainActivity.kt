c8fd480c72007742bdb6423ca69987d828092d98
soyesperoni <hola@dayronesperon.com>
2026-03-18 08:19:06 -0500

    Fix Android biometric auth activity type

--- flutter/android/app/src/main/kotlin/net/supplymax/trust_mobile/MainActivity.kt
+++ flutter/android/app/src/main/kotlin/net/supplymax/trust_mobile/MainActivity.kt
@@ -1,5 +1,5 @@
 package net.supplymax.trust_mobile
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
